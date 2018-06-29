23029d9b4b56c726a67cfcd8e7846f752860d014
J. Félix Ontañón <felix.ontanon@podgroup.com>
2018-06-29 17:57:59 +0200

    Added acronyms for geographical regions

--- plmnutils.js
+++ plmnutils.js
@@ -1,19 +1,11 @@
+// Regions
+const regionList = require('./regions.json');
+const regions = regionsList.map(function(x) {return {[x.code]: x.name}});
 
 // Aux functions for left and right padding strings
 
 function lpad (s,n,c) {var i; var a = s.split(''); for (i = 0; i < n - s.length; i++) {a.unshift (c)}; return a.join('')}
 function rpad (s,n,c) {var i; var a = s.split(''); for (i = 0; i < n - s.length; i++) {a.push (c)}; return a.join('')}
-
-var regions = {
-  "0": "Test networks",
-  "2": "Europe",
-  "3": "North America and the Caribbean",
-  "4": "Asia and the Middle East",
-  "5": "Oceania",
-  "6": "Africa",
-  "7": "South and Central America",
-  "9": "Worldwide"
-};
 
 // PLMN encoding decoding
 // Converts MCC MNC pair to a nibbled PLMN according to 3GPP TS 24.008 standard and vice versa (see section 10.5.1.13)
